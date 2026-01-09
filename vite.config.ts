7b88de7382ebaac00edd790ee2a31b274daa04ee
claudiahar <92089636+claudiahar@users.noreply.github.com>
2026-01-10 00:50:47 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,13 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: "dist",
   },
 });
